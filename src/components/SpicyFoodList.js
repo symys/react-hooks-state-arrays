4b848780e8b54bd66c99169b6d9618da241b181d
SeymaDemir <seymademir35@gmail.com>
2022-11-27 08:04:17 +0300

    completed- there is a question about update

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -6,11 +6,36 @@ function SpicyFoodList() {
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
+    const newFoodArray = [...foods, newFood]
+   setFoods(newFoodArray)
+  }
+
+  function handleLiClick(id){
+    //remove the food when clicked
+    //const cleanArray = foods.filter((food) => food.id !== id)
+    //setFoods(cleanArray)
+
+    //update the food heat level when clicked
+    const newFoodArray = foods.map((food) => {
+      if(food.id === id){
+        //this salving also works
+        //why we need to create a copy of food
+          //food.heatLevel = food.heatLevel + 1
+          //return food
+
+         return{ ...food,
+        heatLevel: food.heatLevel + 1}
+      }
+      else{
+        return food
+      }
+    })
+
+    setFoods(newFoodArray)
   }
 
   const foodList = foods.map((food) => (
-    <li key={food.id}>
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
